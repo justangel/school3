083ba44afab71a6086cfbc3d83d44154ce12926b
justangel <admin@just-gaming.ru>
2017-10-01 18:03:27 +0300

    added additional checks to the TestLanguagesCept.php

--- tests/acceptance/TestLanguagesCept.php
+++ tests/acceptance/TestLanguagesCept.php
@@ -5,9 +5,11 @@ $I->amOnPage('http://localhost:81/');
 $I->see('Видео школа');
 
 $I->amOnPage('http://localhost:81/en/');
+$I->dontSee('Добро пожаловать!');
 $I->see('Welcome!');
 $I->see('Русский', 'a');
 
 $I->amOnPage('http://localhost:81/ru/');
+$I->dontSee('Welcome!');
 $I->see('Добро пожаловать!');
 $I->see('English', 'a');
